15eeb897694feefa7e783583da951c6c91f7c667
janFrystak <janfrystak@gmail.com>
2024-04-10 16:35:20 +0200

    Playing queu, next song, play/stop doesn't work, need work

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -7,42 +7,23 @@ import java.io.FileNotFoundException;
 import java.security.spec.RSAOtherPrimeInfo;
 
 public class MusicPlayer {
-    private static File location;
-    private static FileInputStream fileInputStream;
-
-    static {
-        try {
-            fileInputStream = new FileInputStream(location);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static AdvancedPlayer player;
-
-    static {
-        try {
-            player = new AdvancedPlayer(fileInputStream);
-        } catch (JavaLayerException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void main(String[] args) {
         BasicGUI gui = new BasicGUI();
         gui.setVisible(true);
 
     }
 
-    public static void PlayMusic(File file) {
+    public static void PlayMusic(File location, boolean toPlay) {
         try {
-            location = file;
-            player.play();
+            FileInputStream fileInputStream = new FileInputStream(location);
+            AdvancedPlayer player = new AdvancedPlayer(fileInputStream);
+            if (toPlay) player.play();
+            else player.close(); //Doesn't work, need to figure out what the hell a java Thread is and how to run stuff in parallel
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
         } catch (JavaLayerException e) {
             System.out.println("Error playing MP3: " + e.getMessage());
         }
     }
-    public static void shutUp(){
-        player.close();
-    }
+
 }
