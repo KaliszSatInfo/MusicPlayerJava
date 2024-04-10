4fd6052068fa2cbffb3cdb56ed859a35acbefc00
Projekt_druhy_rocnik <jan.kalisz007@gmail.com>
2024-04-10 13:11:10 +0200

    Done basic cleaning code; Both basic functions merged; Main class deleted;

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BasicGUI gui = new BasicGUI();
-        gui.setContentPane(gui.panel);
-        gui.setVisible(true);
-    }
-}
