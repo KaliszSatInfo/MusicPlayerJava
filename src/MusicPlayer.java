5a61eba1d86116b115eda31fcea6d1a55485d8b7
janka <jan.kalisz007@gmail.com>
2024-04-21 18:05:26 +0200

    Cthulu once, Cthulu forever

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -4,21 +4,18 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.security.spec.RSAOtherPrimeInfo;
 
 public class MusicPlayer extends Thread {
-    /*public static void main(String[] args) {
-        BasicGUI gui = new BasicGUI();
-        gui.setVisible(true);
-
-    }*/
+    private AdvancedPlayer player;
+    private volatile boolean isPlaying;
+    private File currentFile;
 
     public void run() {
         try {
-            FileInputStream fileInputStream = new FileInputStream(BasicGUI.getPlayListIndex());
-            AdvancedPlayer player = new AdvancedPlayer(fileInputStream);
+            FileInputStream fileInputStream = new FileInputStream(currentFile);
+            player = new AdvancedPlayer(fileInputStream);
             player.play();
-            //else player.close(); //Doesn't work, need to figure out what the hell a java Thread is and how to run stuff in parallel
+            isPlaying = true;
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         } catch (JavaLayerException e) {
@@ -26,4 +23,29 @@ public class MusicPlayer extends Thread {
         }
     }
 
+    public void stopPlaying() {
+        if (player != null) {
+            player.close();
+            isPlaying = false;
+        }
+    }
+
+    public void setCurrentFile(File file) {
+        currentFile = file;
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public void startPlaying() {
+        if (player != null && !isPlaying) {
+            try {
+                player.play();
+                isPlaying = true;
+            } catch (JavaLayerException e) {
+                System.out.println("Error playing MP3: " + e.getMessage());
+            }
+        }
+    }
 }
